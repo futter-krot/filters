c124d6a5fab4330d52474d0b7ee0005922213ac8
futter-krot <66520330+futter-krot@users.noreply.github.com>
2021-07-30 18:39:06 +0300

    Update settings.py

--- finale/settings.py
+++ finale/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sy^dgm9#2jvzpd-2^l%_p_jt@4q!v1gpan!n#*!isw93#1x45j'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
